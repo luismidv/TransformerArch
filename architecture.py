a62c9dabc3b40bd56fa5c29817e57cfa93894794
luismidv <perezmartluismiguel@gmail.com>
2024-12-18 10:27:49 +0100

    Last version on datacamp

--- architecture.py
+++ architecture.py
@@ -27,7 +27,7 @@ class MultiHeadAttention(nn.Module):
         self.W_output = nn.Linear(model_dimension,model_dimension)
 
     def scale_dotproduct_attention(self,query, key, values, mask = None):
-        atention_scores = torch.matmul(query,key.transpose(-2,1)) / math.sqrt(self.d_k)
+        atention_scores = torch.matmul(query, key.transpose(-2,1)) / math.sqrt(self.d_k)
         if mask is not None:
             atention_scores = atention_scores.masked_fill(mask == 0, -1e9)
 
@@ -45,11 +45,12 @@ class MultiHeadAttention(nn.Module):
         return x.tranpose(1,2).contiguous().view(batch_size, seq_length, self.model_dimension)
     
     def forward(self,query,key,value,mask = None):
+        #LINEAR TRANSFORMATION AND SPLIT INTO HEADS
         query = self.split_heads(self.W_query(query))
         key = self.split_heads(self.W_key(key))
         value = self.split_heads(self.W_value(value))
 
-        atention_output = self.scaled_dot_product_attention(query,key,value,mask)
+        atention_output = self.scale_dotproduct_attention(query,key,value,mask)
 
         output = self.W_o(self.combine_heads(atention_output))
         return output
@@ -73,8 +74,8 @@ class PositionEncoding(nn.Module):
         pe = torch.zeros(max_seq_length,model_dimension)
         position = torch.arange(0,max_seq_length, dtype = torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, model_dimension, 2).float()) * -(math.log(10000.0))
-        pe = [:, 0::2] = torch.sin(position*div_term)
-        pe = [:, 1::2] = torch.cos(position*div_term)
+        pe[:, 0::2] = torch.sin(position*div_term)
+        pe[:, 1::2] = torch.cos(position*div_term)
 
         #REGISTERED AS A BUFFER, PART OF THE MODULE BUT WONT BE CONSIDERED AS PARAMETER
         self.register_buffer('pe', pe.unsqueeze(0))
@@ -83,7 +84,7 @@ class PositionEncoding(nn.Module):
         return x + self.pe[:, :x.size(1)]
     
 
-class Encoder(nn.Moudle):
+class Encoder(nn.Module):
     def __init__(self, model_dimension, num_heads, d_ff, dropout):
         super(Encoder, self).__init__()
         self.multi_head = MultiHeadAttention(model_dimension, num_heads)
@@ -100,7 +101,7 @@ class Encoder(nn.Moudle):
 
 class Decoder(nn.Module):
     def __init__(self,model_dimension, num_heads, d_ff, dropout):
-        super(DecoderLayer, self).__init__()
+        super(Decoder, self).__init__()
         self.multi_head = MultiHeadAttention(model_dimension, num_heads)
         self.cross_head = MultiHeadAttention(model_dimension, num_heads)
         self.position_ff = PositionWiseFeedForward(model_dimension, d_ff)
@@ -118,9 +119,9 @@ class Decoder(nn.Module):
         x = self.norm3(x + self.dropout_layer(ff_output))
         return x
     
-class Transformer(nn.Module):
+class classTranf(nn.Module):
     def __init__(self, src_vocab_size, tgt_vocab_size, model_dimension, num_heads, num_layers,d_ff,max_seq_length, dropout):
-        super(Transformer,self).__init__()
+        super(classTranf,self).__init__()
         self.encoder_embedding = nn.Embedding(src_vocab_size, model_dimension)
         self.decoder_embedding = nn.Embedding(tgt_vocab_size, model_dimension)
         self.positional_encoding = PositionEncoding(model_dimension, max_seq_length)
@@ -139,10 +140,49 @@ class Transformer(nn.Module):
         tgt_mask = tgt_mask & nopeak_mask
         return src_mask, tgt_mask
     
-    def forward(self, src, tgt)
+    def forward(self, src, tgt):
         src_mask, tgt_mask = self.generate_mask(src,tgt)
         src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
         tgt_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(tgt)))
+        enc_output = src_embedded
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+        dec_output = tgt_embedded
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+        
+        output = self.fc(dec_output)
+        return output
+
+src_vocab_size = 5000
+tgt_vocab_size = 5000
+
+d_model = 512
+num_heads = 8
+num_layers = 6
+d_ff = 2048
+max_seq_length = 100
+dropout = 0.1
+
+trf = classTranf(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
+
+src_data = torch.randint(1,src_vocab_size, (64,max_seq_length)) #(batch_size,seq_length)
+tgt_data = torch.randint(1,tgt_vocab_size, (64,max_seq_length)) #(batch_size,seq_length)
+
+
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.Adam(trf.parameters(), lr = 0.0001, betas=(0.9, 0.98), eps = 1e-9)
+
+trf.train()
+for epoch in range(100):
+    optimizer.zero_grad()
+    output = trf(src_data, tgt_data[:,:-1])
+    loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
+    loss.backward()
+    optimizer.step()
+    print(f"Epoch : {epoch +1}, Loss: {loss.item()}")
+
 
         
 
